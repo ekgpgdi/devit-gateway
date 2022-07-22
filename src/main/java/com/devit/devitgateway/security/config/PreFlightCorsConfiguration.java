fcde5f0cf2346dd0250a8e954731d2537f588366
dahye <>
2022-07-23 05:26:00 +0900

    feat: cors

--- src/main/java/com/devit/devitgateway/security/config/PreFlightCorsConfiguration.java
+++ src/main/java/com/devit/devitgateway/security/config/PreFlightCorsConfiguration.java
@@ -14,33 +14,57 @@ import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Configuration
-public class CorsConfiguration {
 
-    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN,token,username,client";
-    private static final String ALLOWED_METHODS = "*";
-    private static final String ALLOWED_ORIGIN = "*";
-    private static final String ALLOWED_EXPOSE = "*";
+public class PreFlightCorsConfiguration {
+
+
+    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type";
+
+    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
+
+    private static final String ALLOWED_ORIGIN = "https://www.devit.shop";
+
+    private static final String ALLOWED_CREDENTIALS = "true";
+
     private static final String MAX_AGE = "3600";
 
     @Bean
+
     public WebFilter corsFilter() {
+
         return (ServerWebExchange ctx, WebFilterChain chain) -> {
+
             ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
+
+            if (CorsUtils.isPreFlightRequest(request)) {
+
                 ServerHttpResponse response = ctx.getResponse();
+
                 HttpHeaders headers = response.getHeaders();
-                headers.set("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+
+                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+
                 headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+
                 headers.add("Access-Control-Max-Age", MAX_AGE);
+
                 headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-                headers.add("Access-Control-Expose-Headers", ALLOWED_EXPOSE);
-                headers.add("Access-Control-Allow-Credentials", "true");
+
+                headers.add("Access-Control-Allow-Credentials", ALLOWED_CREDENTIALS);
+
                 if (request.getMethod() == HttpMethod.OPTIONS) {
+
                     response.setStatusCode(HttpStatus.OK);
+
                     return Mono.empty();
+
                 }
+
             }
+
             return chain.filter(ctx);
+
         };
+
     }
 }
