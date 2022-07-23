f5aa3b4c6b420ddbbfea7f677c3aadda206b5bf5
dahye <>
2022-07-23 17:16:12 +0900

    feat: cors

--- src/main/java/com/devit/devitgateway/DevitGatewayApplication.java
+++ src/main/java/com/devit/devitgateway/DevitGatewayApplication.java
@@ -1,14 +1,17 @@
 package com.devit.devitgateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@Slf4j
 public class DevitGatewayApplication {
 
     public static void main(String[] args) {
+        log.info("ㅂㅐ포 테스트");
         SpringApplication.run(DevitGatewayApplication.class, args);
     }
 
